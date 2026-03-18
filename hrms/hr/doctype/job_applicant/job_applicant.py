9b60c2fb0b6e980dfe819f5db27e16cf17631f32
Sandesh-ots <sandesh.hegde@ortusolis.com>
2026-03-18 18:07:26 +0530

    Update job_applicant.py

--- hrms/hr/doctype/job_applicant/job_applicant.py
+++ hrms/hr/doctype/job_applicant/job_applicant.py
@@ -1,21 +1,12 @@
-# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
-# License: GNU General Public License v3. See license.txt
-
-# For license information, please see license.txt
-
-
 import frappe
 from frappe import _
 from frappe.model.document import Document
 from frappe.model.naming import append_number_if_name_exists
 from frappe.utils import flt, validate_email_address
-
 from hrms.hr.doctype.interview.interview import get_interviewers
-
 
 class DuplicationError(frappe.ValidationError):
 	pass
-
 
 class JobApplicant(Document):
 	def onload(self):
@@ -25,133 +16,25 @@ class JobApplicant(Document):
 
 	def autoname(self):
 		self.name = self.email_id
-
-		# applicant can apply more than once for a different job title or reapply
 		if frappe.db.exists("Job Applicant", self.name):
 			self.name = append_number_if_name_exists("Job Applicant", self.name)
 
 	def validate(self):
-		if self.email_id:
-			validate_email_address(self.email_id, True)
+		validate_email_address(self.email_id)
+		self.check_for_duplicate()
 
-		if self.employee_referral:
-			self.set_status_for_employee_referral()
+	def check_for_duplicate(self):
+		if self.is_new():
+			duplicate = frappe.db.get_value("Job Applicant", {"email_id": self.email_id, "job_title": self.job_title})
+			if duplicate:
+				frappe.throw(_("Job Applicant with email {0} already exists for {1}").format(self.email_id, self.job_title), DuplicationError)
 
-		if not self.applicant_name and self.email_id:
-			guess = self.email_id.split("@")[0]
-			self.applicant_name = " ".join([p.capitalize() for p in guess.split(".")])
-
-	def before_insert(self):
-		if self.job_title:
-			job_opening_status = frappe.db.get_value("Job Opening", self.job_title, "status")
-			if job_opening_status == "Closed":
-				frappe.throw(
-					_("Cannot create a Job Applicant against a closed Job Opening"), title=_("Not Allowed")
-				)
-
-	def after_insert(self):
-		subject = f"Job Application submitted by {self.applicant_name}"
-		message = f"""
-		Dear HR,<br><br>
-		A new job application submitted.<br><br>
-        
-        <b>Application Details:</b><br>
-        <b>Name:</b> {self.applicant_name}<br>
-        <b>Email:</b> {self.email_id}<br>
-        <b>Position Applied:</b> {self.designation}<br><br>
-        
-        Please log in to the system to view and take necessary action.<br><br>
-
-		"""
-
-		frappe.sendmail(
-			recipients = "meena.bhatia@ortusolis.com",
-			subject=subject,
-			message=message
-			)
-
-	def set_status_for_employee_referral(self):
-		emp_ref = frappe.get_doc("Employee Referral", self.employee_referral)
-		if self.status in ["Open", "Replied", "Hold"]:
-			emp_ref.db_set("status", "In Process")
-		elif self.status in ["Accepted", "Rejected"]:
-			emp_ref.db_set("status", self.status)
-
-
-@frappe.whitelist()
-def create_interview(doc, interview_round):
-	import json
-
-	if isinstance(doc, str):
-		doc = json.loads(doc)
-		doc = frappe.get_doc(doc)
-
-	round_designation = frappe.db.get_value("Interview Round", interview_round, "designation")
-
-	if round_designation and doc.designation and round_designation != doc.designation:
-		frappe.throw(
-			_("Interview Round {0} is only applicable for the Designation {1}").format(
-				interview_round, round_designation
-			)
-		)
-
-	interview = frappe.new_doc("Interview")
-	interview.interview_round = interview_round
-	interview.job_applicant = doc.name
-	interview.designation = doc.designation
-	interview.resume_link = doc.resume_link
-	interview.job_opening = doc.job_title
-
-	interviewers = get_interviewers(interview_round)
-	for d in interviewers:
-		interview.append("interview_details", {"interviewer": d.interviewer})
-
-	return interview
-
+	def on_update(self):
+		if self.status == "Open" and self.job_title:
+			job_opening = frappe.get_doc("Job Opening", self.job_title)
+			if job_opening.status == "Closed":
+				frappe.throw(_("Job Opening {0} is closed").format(self.job_title))
 
 @frappe.whitelist()
 def get_interview_details(job_applicant):
-	interview_details = frappe.db.get_all(
-		"Interview",
-		filters={"job_applicant": job_applicant, "docstatus": ["!=", 2]},
-		fields=["name", "interview_round", "scheduled_on", "average_rating", "status"],
-	)
-	interview_detail_map = {}
-	meta = frappe.get_meta("Interview")
-	number_of_stars = meta.get_options("average_rating") or 5
-
-	for detail in interview_details:
-		detail.average_rating = detail.average_rating * number_of_stars if detail.average_rating else 0
-
-		interview_detail_map[detail.name] = detail
-
-	return {"interviews": interview_detail_map, "stars": number_of_stars}
-
-
-@frappe.whitelist()
-def get_applicant_to_hire_percentage():
-	frappe.has_permission("Job Applicant", throw=True)
-
-	total_applicants = frappe.db.count("Job Applicant")
-	total_hired = frappe.db.count("Job Applicant", filters={"status": "Accepted"})
-
-	return {
-		"value": flt(total_hired) / flt(total_applicants) * 100 if total_applicants else 0,
-		"fieldtype": "Percent",
-	}
-
-@frappe.whitelist()
-def send_internship_details(job_applicant, data):
-    doc = frappe.get_doc("Job Applicant", job_applicant)
-
-    html_template = f"""
-    <p>{data.replace('\n', '<br>')}</p>
-    """
-
-    frappe.sendmail(
-        recipients=[doc.email_id],
-        subject="Internship Details",
-        message=html_template,
-        delayed=False
-    )
-
+	return frappe.get_all("Interview", filters={"job_applicant": job_applicant}, fields=["name", "interview_round", "status"])
